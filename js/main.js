ced8aaa2abf271af7cab362a796977c776735deb
Anton Shchukin <cypher76@gmail.com>
2014-04-11 02:20:17 +0700

    Reviewed code

--- js/main.js
+++ js/main.js
@@ -5,3 +5,23 @@
 $(document).on('ready', function(){
     $('.no-placeholder input[placeholder], .no-placeholder textarea[placeholder]').placeholder();
 });
+
+
+/*
+ * Fancybox
+ */
+
+$(document).on('ready', function(){
+
+    $('.gallery__item__link').fancybox();
+
+});
+
+$(document).on('ready', function(){
+
+    setInterval(function(){
+        $('.billboard__slider :first-child').fadeOut()
+            .next('.billboard__slider__item').fadeIn()
+            .end().appendTo('.billboard__slider');},
+    5000);
+});
